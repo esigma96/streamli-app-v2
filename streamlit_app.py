41947e25c7f2a285d054d5c1d19d3d448cddc0f5
Chima Eze <84285988+esigma96@users.noreply.github.com>
2023-08-18 16:02:20 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+
+# Page title
+st.set_page_config(page_title='🦜🔗 Ask the Doc App')
+st.title('🦜🔗 Ask the Doc App')
+
+# File upload
+uploaded_file = st.file_uploader('Upload an article', type='txt')
+submitted = st.form_submit_button('Submit', disabled=not(uploaded_file))
+
