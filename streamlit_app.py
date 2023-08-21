3f3348833fa18b73bee0825e14fcedede0e22342
Chima Eze <84285988+esigma96@users.noreply.github.com>
2023-08-21 07:11:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ st.title('🦜🔗 WhatsApp Chat Analysis')
 # File upload
 uploaded_file = st.file_uploader('Upload an article', type='txt')
 with st.form('myform', clear_on_submit=True):
-    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file))
+    submitted = st.form_submit_button('Run Analysis', disabled=not(uploaded_file))
 
