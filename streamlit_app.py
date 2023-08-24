82b620d5dca5591a1f24f6a4982f286ad601cfcc
Chima Eze <84285988+esigma96@users.noreply.github.com>
2023-08-24 10:56:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,9 @@ with st.form('myform', clear_on_submit=True):
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
 
     # To convert to a string based IO:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
 
     # To read file as string:
     string_data = stringio.read()
@@ -33,7 +31,8 @@ if uploaded_file is not None:
     df['date'] = pd.to_datetime(df['date'])
     df['time'] = pd.to_timedelta(df['time']+':00')
     df['datetime']= df["date"] + df['time']
+    st.write(df.head(10))
 
 
 
-st.write(df.head(10))
+
