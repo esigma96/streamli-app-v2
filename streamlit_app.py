774565e9fe1ee44ad7d0a92f549cfdc10193c418
Chima Eze <84285988+esigma96@users.noreply.github.com>
2023-08-24 14:59:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,40 +39,40 @@ if uploaded_file is not None:
     #st.dataframe(df['message'])
     # create two columns for charts
     fig_col1, fig_col2 = st.columns(2)
-        with fig_col1:
-            ##Finding pattern of messager
-            pattern = re.compile('\d+:\d+\s+-\s+([a-zA-Z0-9]+\s?[a-zA-Z0-9]+\s?[a-zA-Z0-9]+\s?):\s+')
-            messengers = re.findall(pattern,string_data)
-            count_messages={}
-            for each in messengers:
-                if each in count_messages.keys():
-                    count_messages[each]+=1
-                else:
-                    count_messages[each]=1
-            message_cnt = pd.DataFrame.from_dict([count_messages])
+    with fig_col1:
+        ##Finding pattern of messager
+        pattern = re.compile('\d+:\d+\s+-\s+([a-zA-Z0-9]+\s?[a-zA-Z0-9]+\s?[a-zA-Z0-9]+\s?):\s+')
+        messengers = re.findall(pattern,string_data)
+        count_messages={}
+        for each in messengers:
+            if each in count_messages.keys():
+                count_messages[each]+=1
+            else:
+                count_messages[each]=1
+        message_cnt = pd.DataFrame.from_dict([count_messages])
+    
+        message_cnt = message_cnt.T.sort_values(by = 0, ascending=False)
+        message_cnt = message_cnt.rename(columns={0: 'Mssg_Count'})
         
-            message_cnt = message_cnt.T.sort_values(by = 0, ascending=False)
-            message_cnt = message_cnt.rename(columns={0: 'Mssg_Count'})
-            
-            #EDA for general understanding of the distribution of the dataset.
-            message_cnt['Names'] = message_cnt.index
-            #sns.set(rc={'figure.figsize':(800,600)})
-            ax =sns.barplot(x = 'Names', y = 'Mssg_Count' ,data = message_cnt.head(5), estimator = sum, palette=("crest"))
-            ax.set_title('Most Active Member of the group', size = 20)
-            ax.set(ylabel='No. of Messages')
-            st.bar_chart(data = message_cnt.head(5), x = 'Names', y = 'Mssg_Count')
-            st.write(ax)
-            
-        with fig_col2:
-            hour_pattern = '(\d+):\d+\s+-\s+\w+\s?\w+?\s?\w+\s?\w+:\s'
-            hours = re.findall(hour_pattern,data)
-            time = pd.DataFrame({'hours':hours})
-            time['count'] = [1]* len(time)
-            time = time.sort_values(by = 'hours', ascending = True)
-            #EDA for general understanding of the distribution of the dataset.
-            st.bar_chart(data = time, x = 'hours', y = 'count')
-            st.write(ax)
-            
-            
-            
-            
+        #EDA for general understanding of the distribution of the dataset.
+        message_cnt['Names'] = message_cnt.index
+        #sns.set(rc={'figure.figsize':(800,600)})
+        ax =sns.barplot(x = 'Names', y = 'Mssg_Count' ,data = message_cnt.head(5), estimator = sum, palette=("crest"))
+        ax.set_title('Most Active Member of the group', size = 20)
+        ax.set(ylabel='No. of Messages')
+        st.bar_chart(data = message_cnt.head(5), x = 'Names', y = 'Mssg_Count')
+        st.write(ax)
+        
+    with fig_col2:
+        hour_pattern = '(\d+):\d+\s+-\s+\w+\s?\w+?\s?\w+\s?\w+:\s'
+        hours = re.findall(hour_pattern,data)
+        time = pd.DataFrame({'hours':hours})
+        time['count'] = [1]* len(time)
+        time = time.sort_values(by = 'hours', ascending = True)
+        #EDA for general understanding of the distribution of the dataset.
+        st.bar_chart(data = time, x = 'hours', y = 'count')
+        st.write(ax)
+        
+        
+        
+        
