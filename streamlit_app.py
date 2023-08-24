280253130b425fb25a97cf8b385a1b791e34b682
Chima Eze <84285988+esigma96@users.noreply.github.com>
2023-08-24 15:13:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ if uploaded_file is not None:
         # create two columns for charts
         fig_col1, fig_col2 = st.columns(2)
         with fig_col1:
-            st.markdown("##### Most Active Member of the group")
+            st.markdown("##### ** :red[Most Active Member of the group]**")
             ##Finding pattern of messager
             pattern = re.compile('\d+:\d+\s+-\s+([a-zA-Z0-9]+\s?[a-zA-Z0-9]+\s?[a-zA-Z0-9]+\s?):\s+')
             messengers = re.findall(pattern,string_data)
