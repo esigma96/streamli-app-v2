eba13c6e193dffc8f0f3f8919cc6d71d08717dd0
Chima Eze <84285988+esigma96@users.noreply.github.com>
2023-08-24 15:00:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ if uploaded_file is not None:
         
     with fig_col2:
         hour_pattern = '(\d+):\d+\s+-\s+\w+\s?\w+?\s?\w+\s?\w+:\s'
-        hours = re.findall(hour_pattern,data)
+        hours = re.findall(hour_pattern,string_data)
         time = pd.DataFrame({'hours':hours})
         time['count'] = [1]* len(time)
         time = time.sort_values(by = 'hours', ascending = True)
