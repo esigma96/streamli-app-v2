4777dce24f21e5dafeee8b050c52b7379ec0cd27
Chima Eze <84285988+esigma96@users.noreply.github.com>
2023-08-24 10:01:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,16 @@ import seaborn as sns
 st.set_page_config(page_title='🦜🔗 WhatsApp Chat Analysis')
 st.title('🦜🔗 WhatsApp Chat Analysis')
 
-st.sidebar.success("Select a demo above.")
 # File upload
 uploaded_file = st.file_uploader('Upload your chat file', type='txt')
 with st.form('myform', clear_on_submit=True):
     submitted = st.form_submit_button('Run Analysis', disabled=not(uploaded_file))
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+
+stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+st.write (def read_file (url):
+  file = open(url,mode='r',encoding="utf8")
+  data = file.read()
+  file.close()
+  data
+
+read_file(url))
