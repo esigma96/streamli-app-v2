a1ffcd4920645d6aa9c627fd4bc94110f8a42689
Chima Eze <84285988+esigma96@users.noreply.github.com>
2023-08-24 14:29:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,10 +51,11 @@ if uploaded_file is not None:
     
     #EDA for general understanding of the distribution of the dataset.
     message_cnt['Names'] = message_cnt.index
-    sns.set(rc={'figure.figsize':(10,6.27)})
-    ax =sns.barplot(x = 'Names', y = 'Mssg_Count' ,data = message_cnt.head(5), estimator = sum, palette=("crest"))
-    ax.set_title('Most Active Member of the group', size = 20)
-    ax.set(ylabel='No. of Messages')
+    #sns.set(rc={'figure.figsize':(10,6.27)})
+    #ax =sns.barplot(x = 'Names', y = 'Mssg_Count' ,data = message_cnt.head(5), estimator = sum, palette=("crest"))
+    #ax.set_title('Most Active Member of the group', size = 20)
+    #ax.set(ylabel='No. of Messages')
+    st.bar_chart(data = message_cnt.head(5), x = 'Names', y = 'Mssg_Count')
 
 
 
