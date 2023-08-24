7f7e356656c52fc7aab3bff06449588053b707bf
Chima Eze <84285988+esigma96@users.noreply.github.com>
2023-08-24 15:10:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,44 +37,45 @@ if uploaded_file is not None:
     df['datetime']= df["date"] + df['time']
     df = df.head(10)
     #st.dataframe(df['message'])
-    # create two columns for charts
-    fig_col1, fig_col2 = st.columns(2)
-    with fig_col1:
-        st.markdown("#### Most Active Member of the group")
-        ##Finding pattern of messager
-        pattern = re.compile('\d+:\d+\s+-\s+([a-zA-Z0-9]+\s?[a-zA-Z0-9]+\s?[a-zA-Z0-9]+\s?):\s+')
-        messengers = re.findall(pattern,string_data)
-        count_messages={}
-        for each in messengers:
-            if each in count_messages.keys():
-                count_messages[each]+=1
-            else:
-                count_messages[each]=1
-        message_cnt = pd.DataFrame.from_dict([count_messages])
-    
-        message_cnt = message_cnt.T.sort_values(by = 0, ascending=False)
-        message_cnt = message_cnt.rename(columns={0: 'Mssg_Count'})
+    with st.container():
+        # create two columns for charts
+        fig_col1, fig_col2 = st.columns(2)
+        with fig_col1:
+            st.markdown("#### Most Active Member of the group")
+            ##Finding pattern of messager
+            pattern = re.compile('\d+:\d+\s+-\s+([a-zA-Z0-9]+\s?[a-zA-Z0-9]+\s?[a-zA-Z0-9]+\s?):\s+')
+            messengers = re.findall(pattern,string_data)
+            count_messages={}
+            for each in messengers:
+                if each in count_messages.keys():
+                    count_messages[each]+=1
+                else:
+                    count_messages[each]=1
+            message_cnt = pd.DataFrame.from_dict([count_messages])
         
-        #EDA for general understanding of the distribution of the dataset.
-        message_cnt['Names'] = message_cnt.index
-        #sns.set(rc={'figure.figsize':(800,600)})
-        ax =sns.barplot(x = 'Names', y = 'Mssg_Count' ,data = message_cnt.head(5), estimator = sum, palette=("crest"))
-        ax.set_title('Most Active Member of the group', size = 20)
-        ax.set(ylabel='No. of Messages')
-        st.bar_chart(data = message_cnt.head(5), x = 'Names', y = 'Mssg_Count')
-        st.write(ax)
-        
-    with fig_col2:
-        st.markdown("#### Which hour of the day were most messages sent?")
-        hour_pattern = '(\d+):\d+\s+-\s+\w+\s?\w+?\s?\w+\s?\w+:\s'
-        hours = re.findall(hour_pattern,string_data)
-        time = pd.DataFrame({'hours':hours})
-        time['count'] = [1]* len(time)
-        time = time.sort_values(by = 'hours', ascending = True)
-        #EDA for general understanding of the distribution of the dataset.
-        st.bar_chart(data = time, x = 'hours', y = 'count')
-        st.write(ax)
-        
-        
-        
-        
+            message_cnt = message_cnt.T.sort_values(by = 0, ascending=False)
+            message_cnt = message_cnt.rename(columns={0: 'Mssg_Count'})
+            
+            #EDA for general understanding of the distribution of the dataset.
+            message_cnt['Names'] = message_cnt.index
+            #sns.set(rc={'figure.figsize':(800,600)})
+            ax =sns.barplot(x = 'Names', y = 'Mssg_Count' ,data = message_cnt.head(5), estimator = sum, palette=("crest"))
+            ax.set_title('Most Active Member of the group', size = 20)
+            ax.set(ylabel='No. of Messages')
+            st.bar_chart(data = message_cnt.head(5), x = 'Names', y = 'Mssg_Count')
+            st.write(ax)
+            
+        with fig_col2:
+            st.markdown("#### Which hour of the day were most messages sent?")
+            hour_pattern = '(\d+):\d+\s+-\s+\w+\s?\w+?\s?\w+\s?\w+:\s'
+            hours = re.findall(hour_pattern,string_data)
+            time = pd.DataFrame({'hours':hours})
+            time['count'] = [1]* len(time)
+            time = time.sort_values(by = 'hours', ascending = True)
+            #EDA for general understanding of the distribution of the dataset.
+            st.bar_chart(data = time, x = 'hours', y = 'count')
+            st.write(ax)
+            
+            
+            
+            
